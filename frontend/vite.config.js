3d221349a4c9ed6079c33ab61906751b4983a599
Shubham Saini <shubham.s@semonks.com>
2026-03-10 16:06:48 +0530

    fix vite config and optimize bundle

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  optimizeDeps: {
+    include: ["react-helmet-async"]
+  },
+
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          helmet: ["react-helmet-async"],
+          socket: ["socket.io-client"]
+        }
+      }
+    }
+  }
 });
